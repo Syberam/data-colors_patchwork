dccf09bddbec63fc66d6a48548ec00f2d3b13d44
sbonnefo persoMac <sbonnefo@student.42.fr>
2019-02-06 10:42:02 +0100

    del test eth app

--- vp_patchwork/eth_patchwork/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EthPatchworkConfig(AppConfig):
-    name = 'eth_patchwork'
--- vp_patchwork/eth_patchwork/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ethTestView
-
-urlpatterns = [
-    path('nb_blocks/', ethTestView, name='eth_test'),
-]
